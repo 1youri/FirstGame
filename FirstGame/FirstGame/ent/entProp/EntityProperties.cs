5cd0736ba9a4f20bc76dc6c0c10387db87f011b2
Youri Schuurmans <yourish@live.nl>
2016-03-11 16:30:46 +0100

    Initial Commit (Create FirstGame)

---
+++ FirstGame/FirstGame/ent/entProp/EntityProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstGame.ent.entProp
+{
+    class EntityProperties
+    {
+        public double MoveSpeed { get; set; }
+    }
+}
